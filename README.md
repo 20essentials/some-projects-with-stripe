b018c296b482e6dbc5c4a1b0672c103faa0825e6
DominicDev <s0l0estudi4r@gmail.com>
2026-02-13 11:28:01 +0100

    v0.0.1

---
+++ README.md
@@ -0,0 +1,7 @@
+[![Foto Preview](preview/project-1327.avif)](https://project-1327.vercel.app/)
+
+<div align="center" style="display: flex; justify-content: center;">
+  <a  href="https://github.com/20essentials/project-1327" target="_blank">&#8592;</a>
+  &nbsp;&nbsp;
+  <a  href="https://github.com/20essentials/project-1327" target="_blank">&#8594;</a>
+</div>
---
+++ src/app/layout.tsx
@@ -0,0 +1,24 @@
+import { type Metadata } from 'next';
+import '@/styles/globalReset.css'
+import '@/styles/global.css';
+import { inter } from '@/lib/fonts';
+
+export const metadata: Metadata = {
+  title: 'Some Projects With Stripe',
+  description: 'Practicing with Stripe',
+  icons: {
+    icon: '/assets/favicon.png'
+  }
+};
+
+export default function RootLayout({
+  children
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang='en' className={inter.className}>
+      <body>{children}</body>
+    </html>
+  );
+}
