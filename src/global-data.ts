e0c41123601ffcadf5b4b6b3f8c01222d3bfcfdb
DominicDev <s0l0estudi4r@gmail.com>
2026-02-25 17:24:08 +0100

    v0.1.6

--- src/global-data.ts
+++ src/global-data.ts
@@ -3,10 +3,16 @@ import type { Route } from 'next';
 type ItemProject = {
   href: Route;
   nameProject: string;
+  realHref?: string;
 };
 
 export const arrayOfProjects: ItemProject[] = [
   { href: '/1', nameProject: 'Recurring Pricing Plans' },
   { href: '/2', nameProject: 'Single Payment' },
-  { href: '/3', nameProject: 'Webhooks' },
+  {
+    href: '/3',
+    nameProject: 'Webhooks',
+    realHref:
+      'https://github.com/20essentials/some-projects-with-stripe/blob/main/src/app/3/api/webhook/route.ts'
+  }
 ] as const;
