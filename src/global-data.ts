8bf3618f3cd6d19388a48cfeee4f22c4bcc24a37
DominicDev <s0l0estudi4r@gmail.com>
2026-03-01 12:47:35 +0100

    v0.2.2

--- src/global-data.ts
+++ src/global-data.ts
@@ -4,16 +4,31 @@ type ItemProject = {
   href: Route;
   nameProject: string;
   realHref?: string;
+  srcImage: string;
 };
 
 export const arrayOfProjects: ItemProject[] = [
-  { href: '/1', nameProject: 'Recurring Pricing Plans' },
-  { href: '/2', nameProject: 'Single Payment' },
+  {
+    href: '/1',
+    nameProject: 'Recurring Pricing Plans',
+    srcImage: '/assets/projects/1.avif'
+  },
+  {
+    href: '/2',
+    nameProject: 'Single Payment',
+    srcImage: '/assets/projects/2.avif'
+  },
+
   {
     href: '/3',
     nameProject: 'Webhooks',
     realHref:
-      'https://github.com/20essentials/some-projects-with-stripe/blob/main/src/app/3/api/webhook/route.ts'
+      'https://github.com/20essentials/some-projects-with-stripe/blob/main/src/app/3/api/webhook/route.ts',
+    srcImage: '/assets/projects/3.avif'
   },
-  { href: '/4', nameProject: 'Creating an Stripe Customer' },
+  {
+    href: '/4',
+    nameProject: 'Creating an Stripe Customer',
+    srcImage: '/assets/projects/4.avif'
+  }
 ] as const;
