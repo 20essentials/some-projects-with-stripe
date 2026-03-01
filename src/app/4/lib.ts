382d5faee450f566191ec686b3432954a9fb6db8
DominicDev <s0l0estudi4r@gmail.com>
2026-03-01 12:43:20 +0100

    v0.2.1

--- src/app/4/lib.ts
+++
@@ -1,8 +0,0 @@
-import { BASE_URL } from '@/globalConsts';
-
-export const PRODUCT_MOCKUP = {
-  name: 'Phone',
-  priceInCents: 20000,
-  urlImage: `${BASE_URL}/assets/phone.png`
-};
-
--- src/app/4/page.tsx
+++ src/app/4/page.tsx
@@ -1,6 +1,4 @@
 import { Formulario } from './componets/formulario';
-import { PayButton } from './componets/PayButton';
-import { PRODUCT_MOCKUP } from './lib';
 
 export default async function Page() {
   return (
@@ -10,16 +8,7 @@ export default async function Page() {
         alt='fondo'
         className='absolute pointer-events-none w-full h-screen object-cover'
       />
-      {/*    <aside className='border-[0.1vmax] border-white-200 border-solid p-[1vmax] flex flex-col gap-1 text-center relative z-10 bg-[#fff2] rounded-[0.8vmax]'>
-        <h3>{PRODUCT_MOCKUP.name}</h3>
-        <img
-          src={PRODUCT_MOCKUP.urlImage}
-          alt='fondo'
-          className='pointer-events-none w-[5vmax] object-cover'
-        />
-        <h4>{(PRODUCT_MOCKUP.priceInCents ?? 0) / 100}$</h4>
-        <PayButton />
-      </aside> */}
+
       <Formulario />
     </section>
   );
