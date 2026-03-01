6e00ead542139084ee4229ad46de164bac26971a
DominicDev <s0l0estudi4r@gmail.com>
2026-03-01 12:42:19 +0100

    v0.2.0

--- src/app/4/action/create-customer.ts
+++ src/app/4/action/create-customer.ts
@@ -1,8 +1,53 @@
 'use server';
-import Stripe from 'stripe';
 
-export const createStripeCustomer = async (formData: FormData) => {
+import Stripe from 'stripe';
+import { CreateStripeCustomerState } from '../types/types';
+
+export const createStripeCustomer = async (
+  prevState: CreateStripeCustomerState,
+  formData: FormData
+): Promise<CreateStripeCustomerState> => {
   const secret = process.env.STRIPE_API_SECRET;
-  if (!secret) throw new Error('Stripe Api Key not defined!');
-  const stripe = new Stripe(secret);
+
+  if (!secret) {
+    return {
+      ...prevState,
+      error: 'Stripe Api Key not defined',
+      success: null
+    };
+  }
+
+  const username = formData.get('username');
+  const email = formData.get('email');
+
+  if (typeof username !== 'string' || typeof email !== 'string') {
+    return {
+      ...prevState,
+      error: 'Invalid form data',
+      success: null
+    };
+  }
+
+  try {
+    const stripe = new Stripe(secret);
+
+    const customerCreated = await stripe.customers.create({
+      email,
+      description: `Customer created for ${username}`
+    });
+
+    return {
+      username,
+      email,
+      success: `Customer created successfully with the ID: ${customerCreated.id}`,
+      error: null
+    };
+  } catch {
+    return {
+      username,
+      email,
+      success: null,
+      error: 'Failed to create customer'
+    };
+  }
 };
