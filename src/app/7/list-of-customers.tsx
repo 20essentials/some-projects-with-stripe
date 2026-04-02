282d5bafb402ee86be464871b138bac964c2c2d5
DominicDev <s0l0estudi4r@gmail.com>
2026-04-02 17:48:59 +0200

    v0.5.0

--- src/app/7/list-of-customers.tsx
+++ src/app/7/list-of-customers.tsx
@@ -5,7 +5,8 @@ export async function ListOfCustomers() {
   const customers = await getCustomers();
 
   return (
-    <div className='flex flex-col gap-4'>
+    <div className='flex flex-col gap-4 w-full h-screen z-20 place-items-center p-[1.2vmax]'>
+      
       {(customers as Customer[]).map(({ id, ...rest }: Customer) => (
         <CustomerItem key={id} customer={{ id, ...rest }} />
       ))}
--- src/app/7/page.tsx
+++ src/app/7/page.tsx
@@ -1,14 +1,18 @@
-
+import { Suspense } from 'react';
+import { ListOfCustomers } from './list-of-customers';
 
 export default async function Page() {
-
   return (
-    <section className='w-full h-screen flex flex-wrap place-content-center gap-[1vmax]'>
+    <section className='w-full h-screen flex relative flex-wrap place-content-center gap-[1vmax]'>
       <img
         src='/assets/bg7.jpeg'
         alt='fondo'
-        className='absolute pointer-events-none w-full h-screen object-cover'
+        className='fixed pointer-events-none w-full h-screen object-cover -z-30'
       />
+      <aside className='fixed w-full h-screen -z-20 bg-[#000a]'></aside>
+      <Suspense fallback={<h2>Loading...</h2>}>
+        <ListOfCustomers />
+      </Suspense>
     </section>
   );
 }
